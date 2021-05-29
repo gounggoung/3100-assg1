220b6db31b291882f293d6cd4255ca46e6240437
Jacob Johnston <gounggoung@hotmail.com>
2021-05-29 17:06:42 +1000

    Start implementation of average turnaround time improvement

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 import sim.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
@@ -12,48 +13,53 @@ public class App {
         Client client = new Client(); // Handshake is handled by constructor
 
         ServerConfig[] currentServerStates = null;
-        ServerConfig[] servers = null;
-        
+        ArrayList<Job> waitingJobs = new ArrayList<>();
 
         // Event loop
         for (DSEvent event = client.getEvent(); event.eventType != DSEvent.EventType.NONE; event = client.getEvent()) {
+           // System.out.println(event.eventType);
+
             switch (event.eventType) {
+
                 case JOB:
                     Job job = (Job) event;
+
                     // Can't do this outside loop, REDY needs to be called before GETS
-
                     currentServerStates = client.getServers("Capable " + job.core + " " + job.memory + " " + job.disk);
-                    if (servers == null) {
 
-                        servers = currentServerStates;
-                        Arrays.sort(servers, new SmallestFirst());
-                        
-                       // System.out.println(Arrays.toString(currentServerStates) + "\n\n\n");
+                    // System.out.println(job.toString());
 
+                    boolean found = false;
+                    for (ServerConfig server : currentServerStates) {
+                        if (server.core >= job.core) {
+                            if(job.jobID == 93){
+                                int test = 3;
+                            }
+                            client.scheduleJob(job.jobID, server.type, server.id);
+                            found = true;
+                            break;
+                        }
                     }
-                    System.out.println(job.toString());
-                    // if (servers[servers.length - 1].core >= job.core) {
-                        Boolean found = false;
-                        for (ServerConfig server : currentServerStates) {
-                            if (server.core >= job.core) {
-                                client.scheduleJob(job.jobID, server.type, server.id);
-                                found = true;
-                                break;
-                            }
-                        }
 
-                        if(!found){
-                            for(ServerConfig server : servers){
-                                if (server.core >= job.core) {
-                                    client.scheduleJob(job.jobID, server.type, server.id);
-                                    break;
-                                }
-                            }
-                        }
-                    // }
+                    if (!found) {
+                        client.scheduleJob(job.jobID, currentServerStates[0].type, currentServerStates[0].id);
+                    }
 
                     break;
                 case COMPLETE:
+                    for (Job waitingJob : waitingJobs) {
+                        currentServerStates = client.getServers("Capable " + waitingJob.core + " " + waitingJob.memory + " " + waitingJob.disk);
+                        for (ServerConfig server : currentServerStates) {
+                            if (server.core >= waitingJob.core) {
+    
+                                client.scheduleJob(waitingJob.jobID, server.type, server.id);
+                                waitingJobs.remove(waitingJob);
+                                break;
+                            }
+                        }
+                    }
+                    break;
+
                 case FAILURE:
                 case RECOVERY:
                     break;
